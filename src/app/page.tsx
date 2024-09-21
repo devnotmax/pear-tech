92d91d34adde517ec22d9eec123415cc2d051adc
devnotmax <develop.maxsj@gmail.com>
2024-09-21 10:35:32 -0300

    prueba home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import HomeContainer from "@/components/home/HomeContainer";
 
 const Home: React.FC = () => {
-  // const images = ["./Banner 1.png", "./Banner 2.png", "./Banner 3.png"];
+  
 
   return (
     <HomeContainer />
